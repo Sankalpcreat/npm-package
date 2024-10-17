b6b32ba1422ec928024de9a655425d71a1769b81
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-17 18:33:27 +0530

    all deletd

--- src/app/api/npm-stats/[package-name]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import npmService from '@/services/npmService'
-
-export async function GET(request:Request,{params}:{params:{packageName:string}}){
-    const packageName=params.packageName;
-    const packageData=await npmService.getPackageData(packageName);
-
-    return NextResponse.json(packageData)
-}
